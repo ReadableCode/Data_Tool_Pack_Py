cb081b1e0c874df9d697e9b1288e798388cb2857
Jason Christiansen <jason.christiansen@hellofresh.com>
2024-11-05 09:39:37 -0600

    add file format converter in case needed again

--- src/utils/password_tools.py
+++ src/utils/password_tools.py
@@ -2,6 +2,7 @@
 # Imports #
 
 import hashlib
+import json
 import os
 import random
 import string
@@ -12,8 +13,8 @@ if __name__ == "__main__":
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-import utils.config_utils  # noqa: F401
-from utils.display_tools import pprint_df, print_logger  # noqa F401
+from utils.config_utils import great_grandparent_dir  # noqa E402
+from utils.display_tools import pprint_df, pprint_dict, print_logger  # noqa F401
 
 # %%
 # Functions #
@@ -49,6 +50,69 @@ def generate_password_and_hash_for_user(email_address):
     print(f"{email_address}")
     print(f"{password}")
     print(f"password_hash: {password_hash}")
+
+
+def convert_password_storage(
+    input_file_path,
+    output_file_path,
+    output_file_path_json,
+    dict_password_hashes_username,
+):
+    dict_return = {}
+    with open(input_file_path, "r") as file:
+        username = ""
+        password = ""
+        password_hash = ""
+
+        for line in file:
+            print("-" * 50)
+            if line.strip() == "":
+                print("blank line, ending file read")
+                break
+
+            print(line)
+            key, value = line.split("=", 1)
+
+            # if line contains "password" then save as password
+            if "password" in key.lower() and "hash" not in key.lower():
+                print("password found")
+                password = value.strip()[1:-1]
+            # if line contains "password_hash" then save as password_hash
+            elif "hash" in key.lower():
+                print("hash found")
+                password_hash = value.strip()[1:-1]
+                username = dict_password_hashes_username[password_hash]
+                print(f"username: {username}")
+
+            if (username != "") and (password != "") and (password_hash != ""):
+                print("saving")
+                dict_return[username] = {}
+                dict_return[username]["password"] = password
+                dict_return[username]["password_hash"] = password_hash
+
+                # reset
+                username = ""
+                password = ""
+                password_hash = ""
+            else:
+                print("error: username, password, or password_hash not found")
+                print(f"username: {username}")
+                print(f"password: {password}")
+                print(f"password_hash: {password_hash}")
+
+    pprint_dict(dict_return)
+
+    with open(output_file_path, "w") as file:
+        for username, password_dict in dict_return.items():
+            file.write(f'{username.upper()}_PASSWORD="{password_dict["password"]}"\n')
+            file.write(
+                f'{username.upper()}_PASSWORD_HASH="{password_dict["password_hash"]}"\n'
+            )
+
+    json.dump(dict_return, open(output_file_path_json, "w"), indent=4)
+
+    print("Conversion complete")
+    return dict_return
 
 
 # %%
