acf46dc8c927e9182c2536da1100738d622b8613
Jason Christiansen <jason.christiansen@hellofresh.com>
2024-12-04 10:16:14 -0600

    add traceback and failures

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,6 +1,8 @@
 # %%
 # Imports #
 
+import traceback
+
 import config_tests  # noqa: F401
 import pandas as pd
 from src.utils.display_tools import pprint_df, print_logger
@@ -11,6 +13,8 @@ from src.utils.display_tools import pprint_df, print_logger
 
 def get_results_of_tests(ls_test_funcs):
     results = {}
+    tracebacks = {}
+    any_failed = False
 
     for func in ls_test_funcs:
         name = func.__name__
@@ -18,7 +22,9 @@ def get_results_of_tests(ls_test_funcs):
             func()
             results[name] = "Success"
         except Exception as e:
+            any_failed = True
             results[name] = f"Error: {e}"
+            tracebacks[name] = traceback.format_exc()
 
     df_results = (
         pd.DataFrame.from_dict(results, orient="index").reset_index().fillna("")
@@ -28,7 +34,14 @@ def get_results_of_tests(ls_test_funcs):
     print_logger("Results", as_break=True)
     pprint_df(df_results)
 
-    print_logger("Done")
+    if any_failed:
+        print_logger("Failures Tracebacks", as_break=True)
+        for test, tb in tracebacks.items():
+            print_logger(f"Traceback for {test}:\n{tb}", as_break=True)
+
+        raise Exception("There were failures in the tests")
+
+    print_logger("Done without failures")
 
 
 # %%
